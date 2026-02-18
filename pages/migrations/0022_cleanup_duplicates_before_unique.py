f148245c264dfbae1368d2c77c33805522c12d41
Felixcanalla <migonepatricio@gmail.com>
2026-02-18 10:52:45 -0300

    2

--- pages/migrations/0022_cleanup_duplicates_before_unique.py
+++ pages/migrations/0022_cleanup_duplicates_before_unique.py
@@ -1,34 +1,17 @@
-from django.db import migrations
-from django.db.models import Count
-
-
-def cleanup_articulo_destino_duplicates(apps, schema_editor):
-    Rel = apps.get_model("pages", "ArticuloDestinoRelation")
-
-    dups = (
-        Rel.objects.values("articulo_id", "destino_id")
-        .annotate(c=Count("id"))
-        .filter(c__gt=1)
-    )
-
-    for row in dups:
-        qs = (
-            Rel.objects.filter(
-                articulo_id=row["articulo_id"],
-                destino_id=row["destino_id"],
-            )
-            .order_by("sort_order", "id")
-        )
-        keep = qs.first()
-        qs.exclude(id=keep.id).delete()
-
+from django.db import migrations, models
 
 class Migration(migrations.Migration):
 
     dependencies = [
-        ("pages", "0021_alter_articulodestinorelation_options_and_more"),
+        ('pages', '0021_alter_articulodestinorelation_options_and_more'),
     ]
 
     operations = [
-        migrations.RunPython(cleanup_articulo_destino_duplicates, migrations.RunPython.noop),
+        migrations.AddConstraint(
+            model_name='articulodestinorelation',
+            constraint=models.UniqueConstraint(
+                fields=('articulo', 'destino'),
+                name='unique_articulo_destino_relation',
+            ),
+        ),
     ]
